5ff88c651567f5f752c3467018115dc712d103ec
ygalanter <yuri@galanter.net>
2018-12-20 11:35:29 -0500

    Updated from Studio

--- app/index.js
+++ app/index.js
@@ -1,53 +1,77 @@
-// importing libraries
-import document from "document";
 import {preferences} from "user-settings";
+import document from "document";
 import dtlib from "../common/datetimelib";
 import {display} from "display";
+import clock from "clock";
+import * as messaging from "messaging";
+import * as fs from "fs";
+import { me } from "appbit";
 
 // getting SVG elements
 let flame = document.getElementById("flame");
-let flameHidden = document.getElementById("flameHidden");
 let time = document.getElementById("time");
-let all = document.getElementById("all");
+
+// get user time format preference
+dtlib.timeFormat = preferences.clockDisplay == "12h" ? 1: 0;
 
 
-// flame animation
-function animateFlame(delay) {
-  let last_time_update = null;
-  let frame = 1;        //starting with frame 1
-  let frameCount = 12;  //total 12 frames
-  return setInterval(function(){
-   
-      // preloading "caching" next frame into hidden IMAGE
-      if (frame < frameCount) {
-        flameHidden.href = `frames/frame${frame+1}.png`;
-      } else {
-        flameHidden.href = `frames/frame${1}.png`;
-      }
-    
-      //loading current frame
-      flame.href = `frames/frame${frame}.png`;
-    
-      //incrementing frame counter
-      frame++;
-      if (frame > frameCount) {
-          frame = 1;
-      }
-
-      var now = new Date();
-      if ((last_time_update == null) || (last_time_update.getMinutes() != now.getMinutes()) || (last_time_update.getHours() != now.getHours())) {
-        last_time_update = now;
-        updateClock(now);
-      }
-      
-    
-  }, delay)
-  
+// on app exit collect settings 
+me.onunload = () => {
+  fs.writeFileSync("user_settings.json", userSettings, "json");
 }
 
 
+// Message is received
+messaging.peerSocket.onmessage = evt => {
+  
+  switch (evt.data.key) {
+    case "timecolor": 
+          userSettings[evt.data.key] = evt.data.newValue.replace(/["']/g, "");
+          time.style.fill = userSettings.timecolor;
+          break;
+  };
+ 
+      
+}
+
+// Message socket opens
+messaging.peerSocket.onopen = () => {
+  console.log("App Socket Open");
+};
+
+// Message socket closes
+messaging.peerSocket.close = () => {
+  console.log("App Socket Closed");
+};
+
+
+
+// trying to get user settings if saved before
+let userSettings;
+try {
+  userSettings = fs.readFileSync("user_settings.json", "json");
+} catch (e) {
+  userSettings = {timecolor: "#000000"}
+}
+
+
+//trap
+if (!userSettings.timecolor) userSettings = {timecolor: "#000000"}
+
+time.style.fill = userSettings.timecolor;
+
+
+
+flame.animate("enable");
+
+// Update the clock every minute
+clock.granularity = "minutes";
+
 // Update the <text> element with the current time
-function updateClock(today) {
+clock.ontick = (evt) => {
+  
+  // getting current date time
+  let today = evt.date;
  
   // formatting hours based on user preferences
   let hours = dtlib.format1224hour(today.getHours());
@@ -60,50 +84,22 @@ function updateClock(today) {
   // getting 0-preprended minutes
   let mins = dtlib.zeroPad(today.getMinutes());
 
-  time.text = `${monoDigits(hours)}:${monoDigits(mins)}`;
+  time.text = `${hours}:${mins}`;
+
 }
 
 
-//creating monospace digits
-function monoDigits(digits) {
-  var ret = "";
-  var str = digits.toString();
-  for (var index = 0; index < str.length; index++) {
-    var num = str.charAt(index);
-    ret = ret.concat(hex2a("0x1" + num));
-  }
-  return ret;
-}
-
-// Hex to string
-function hex2a(hex) {
-  var str = '';
-  for (var index = 0; index < hex.length; index += 2) {
-    var val = parseInt(hex.substr(index, 2), 16);
-    if (val) str += String.fromCharCode(val);
-  }
-  return str.toString();
-}
 
 // on display on/off stoping/starting animation/time
 display.onchange = () => {
   if (display.on) {
-    console.log("display on")
-    interval = animateFlame(50);  //restarting flame animation with 50ms delay between frames
-    setTimeout(function(){all.style.visibility="visible"}, 50); //to avoid skipped frame making displat visible on 50ms delay
+    flame.animate("enable");
   } else {
-    console.log("display off")
-    all.style.visibility="hidden";
-    clearInterval(interval);
+    flame.animate("disable");
   }
 }
 
 
 
-// get user time format preference
-dtlib.timeFormat = preferences.clockDisplay == "12h" ? 1: 0;
 
 
-let interval = animateFlame(50); //kicking of flame animation with 50ms delay between frames
-setTimeout(function(){all.style.visibility="visible"}, 50); //to avoid skipped frame making displat visible on 50ms delay
-console.log("kicked interval");
